2473a430e0be91f8a56b03badbd1876c1dbb4bce
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-02-19 19:18:54 -0500

    Installed Axios for React + Modified Apps.js to set up axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs();
+  }, []);
+
+  async function getAllSongs(){
+    const response = await axios.get('http://127.0.0.1:8000/api/music/');
+    console.log(response.data)
+    setSongs(response.data)
+  }
+
   return (
     <div className="App">
-        <div>Hellow World</div>
+        <button onClick={() => getAllSongs()}> GET ALL SONGS</button>
     </div>
   );
 }
